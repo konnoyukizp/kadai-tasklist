403713565c0f5f4843b7643b4a39e6be198b04ae
yuuki.konno <konno.yuki.zp@toinx.co.jp>
2020-12-15 23:48:48 +0000

    kadai-tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -48,8 +48,15 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+    // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+    
         // タスクを作成
         $task = new task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -100,9 +107,17 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         // idの値でタスクを検索して取得
         $task = task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
